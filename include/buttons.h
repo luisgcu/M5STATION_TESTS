291418543fb0fb70efc4b226c855ee921c78f1d7
luisgcu <angelgcu@gmail.com>
2023-06-05 20:52:49 -0400

    org

--- include/buttons.h
+++ include/buttons.h
@@ -1,3 +1,64 @@
-void buttons_setup(){
-    //nothing here
+void btn_task(void *arg);
+bool once2 = true;
+#pragma once
+#include <EasyButton.h>
+// ButtonA
+EasyButton buttonA(BTN_A_PIN);
+// ButtonB
+EasyButton buttonB(BTN_B_PIN);
+// ButtonB
+EasyButton buttonC(BTN_C_PIN);
+
+// Callback function to be called when button1 is pressed
+void onButtonAPressed()
+{
+    Serial2.println("ButtonA pressed");
+}
+void onButtonBPressed()
+{
+    Serial2.println("ButtonB pressed");
+}
+void onButtonCPressed()
+{
+    Serial2.println("ButtonC pressed");
+}
+
+void buttons_setup()
+{
+
+    // Initialize the buttons and Add the callback function to be called when the button2 is pressed.
+    buttonA.begin();
+    buttonB.begin();
+    buttonC.begin();    
+    buttonA.onPressed(onButtonAPressed);   
+    buttonB.onPressed(onButtonBPressed);
+    buttonC.onPressed(onButtonCPressed);
+    // nothing here
+    pinMode(BTN_A_PIN, INPUT_PULLUP);
+    pinMode(BTN_B_PIN, INPUT_PULLUP);
+    pinMode(BTN_C_PIN, INPUT_PULLUP);
+    xTaskCreate(btn_task,   /* Task function. */
+                "btn_task", /* String with name of task. */
+                8096,       /* Stack size in bytes. */
+                NULL,       /* Parameter passed as input of the task */
+                1,          /* Priority of the task. */
+                NULL);      /* Task handle. */
+}
+
+void btn_task(void *arg) // task runs on core 0
+{
+    while (1)
+    {
+
+        buttonA.read();
+        buttonB.read();
+        buttonC.read();
+        vTaskDelay(200);
+        if (once2 == true)
+        {
+            Serial2.print("btn_task running on core ");
+            Serial2.println(xPortGetCoreID());
+        }
+        once2 = false;
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,32 @@
 #include "display.h"
 #include "buttons.h"
 #include "pixelleds.h"
+#include <EasyButton.h>
+#include "Ticker.h"
 // put function declarations here:
-
-
-
-
-void setup() {
+bool once = true;
+void setup()
+{
   // put your setup code here, to run once:
   M5.begin();
-  Serial.begin(1115200);
+  pinMode(PORT485_DE_RE, OUTPUT);
+  digitalWrite(PORT485_DE_RE, HIGH); // DE&RE
+  Serial2.begin(115200);
+  Serial2.print("setup() running on core ");
+  Serial2.println(xPortGetCoreID());
   display_setup();
   pixel_setup();
   buttons_setup();
-  
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  if (once == true)
+  {
+    Serial2.print("Loop() running on core ");
+    Serial2.println(xPortGetCoreID());
+    
+  }
+  once = false;
 }
-
